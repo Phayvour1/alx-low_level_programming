68fa87182da1254db1ee2289bb73e22b44d18cdc
favour <pharlorlah700@gmail.com>
2022-10-13 07:23:29 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,10 @@
 /**
  *
  * main - Entry point*
- * Return: Always 0*
+ * Return: Always 0
  */
 int main(void)
 {
-
  	int a;
 	long int b;
 	long long int c;
@@ -18,5 +17,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-
 }
