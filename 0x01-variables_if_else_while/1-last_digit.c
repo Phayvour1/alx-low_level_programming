e17c594c23c83f9033747ecffc7758be72ff310f
favour <pharlorlah700@gmail.com>
2022-10-14 21:51:45 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,14 +5,10 @@
 #include <time.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0
- *
- *     */
+ * Return: Always 0
+ */
 
 int main(void)
 {
