f284447e98960d036a7e7c558514353f8d1c4969
favour <pharlorlah700@gmail.com>
2022-10-14 21:36:11 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,7 @@ srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-
 		printf("%d is positive\n", n);
-
 
 	}
 	else if (n == 0)
@@ -27,6 +25,7 @@ srand(time(0));
 	}
 	else
 	{
+	
 		printf("%d is negative\n", n);
 
 	}
