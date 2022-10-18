007acb0f955a762e3469fe026b0d14657252b935
favour <pharlorlah700@gmail.com>
2022-10-17 21:09:34 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,27 +5,17 @@
 #include <time.h>
 
 /**
- *
- *  * main - entry point
- *
- *   * Return: Always 0
- *
- *    */
+ * main - entry point
+ * Return: Always 0
+ */
 
 int main(void)
 
 {
+	int i;
 
-		int i;
-
-
-
-			for (i = '0'; i <= '9'; i++)
-
-					putchar(i);
-
-				putchar('\n');
-
-					return (0);
-
-						}
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
