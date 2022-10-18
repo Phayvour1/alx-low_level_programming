90b01f14a2002ddb6f025ff7fcdd43b7a16f08c8
favour <pharlorlah700@gmail.com>
2022-10-17 21:02:08 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -12,16 +12,9 @@
 int main(void)
 
 {
-
-		int i;
-
-
-
-		for (i = '0'; i <= '9'; i++)
-
-					putchar(i);
-
-			putchar('\n');
-
-					return (0);
+	int i;
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
